a8e426111706e808523ed8c24dc6cdbdfc3c9b07
Charyzzz <leosamillan82@gmail.com>
2025-09-06 12:28:10 -0500

    Add files via upload

---
+++ programa_base/mapa_campimetrico_prueba2.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+def leer_datos_json(archivo):
+    """Lee los datos de un archivo JSON y devuelve una matriz de 8x9 con los valores de Total Deviation"""
+    with open(archivo, 'r') as f:
+        json_data = json.load(f)
+    
+    matriz = np.zeros((8, 9))  # Matriz de 8x9 (filas Y, columnas X), inicialmente con ceros
+    
+    # Recorrer los datos y llenar la matriz
+    for y in range(8):
+        for x in range(9):
+            valor = json_data['td'][y][x]
+            
+            # Asignar NaN a las zonas fuera del rango (100.0)
+            if valor == 100.0:
+                matriz[y, x] = np.nan  # Marcar como NaN (fuera de rango)
+            else:
+                matriz[y, x] = valor  # Colocamos el valor de dB en la matriz
+
+    return matriz
+
+def graficar_mapa(matriz):
+    """Genera un mapa de calor basado en la matriz de Total Deviation con ciertos recuadros en blanco"""
+    fig, ax = plt.subplots()
+    
+    # Usamos el colormap 'coolwarm' y configuramos NaN para ser blanco
+    cmap = plt.get_cmap('coolwarm')
+    cmap.set_bad('white')  # Los valores NaN (fuera de rango) serán blancos
+    
+    # Crear el gráfico con el colormap y mostrar los valores NaN en blanco
+    cax = ax.imshow(matriz, cmap=cmap, interpolation='nearest', aspect='auto', vmin=-35, vmax=5)
+    
+    # Añadir colorbar
+    cbar = fig.colorbar(cax)
+    cbar.set_label("Total Deviation (dB)", rotation=270, labelpad=15)
+    
+    # Títulos y etiquetas
+    ax.set_title("Mapa Campimétrico - Total Deviation")
+    ax.set_xlabel("Coordenada X (0-8)")
+    ax.set_ylabel("Coordenada Y (0-7)")
+    
+    # Configurar los ticks para que coincidan con las coordenadas
+    ax.set_xticks(np.arange(-0.5, 9, 1), minor=True)
+    ax.set_yticks(np.arange(-0.5, 8, 1), minor=True)
+    ax.grid(which="minor", color="black", linestyle='-', linewidth=0.5)
+    ax.set_xticks(np.arange(0, 9))
+    ax.set_yticks(np.arange(0, 8))
+    
+    # Mostrar el gráfico
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    archivo = 'datos.json'  # Asegúrate de especificar el nombre correcto del archivo
+    matriz = leer_datos_json(archivo)  # Lee los datos del JSON
+    graficar_mapa(matriz)  # Graficar el mapa con los datos
+
+if __name__ == "__main__":
+    main()
